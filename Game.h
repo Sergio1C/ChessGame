5e091848d1c3ad1e20085dd523d6714fc8e4d159
Sergio1C <nofxsss@gmail.com>
2016-04-26 18:44:51 +0300

    1

--- Game.h
+++ Game.h
@@ -9,6 +9,7 @@ enum Game_type { chess, barley};
 class Game{
 
 public:
+	Game();
 	Game(Game_type game_type);
 	virtual void move(Piece&) = 0;
 
@@ -19,3 +20,26 @@ private:
 	int _count_black;
 
 };
+
+class Chess_Game{
+public:
+	Chess_Game(); 
+};
+
+class Barley_Game{
+public:
+	Barley_Game();
+};
+
+Game::Game()
+{
+	int choose;
+	std::cout << "Choose the game type:(chess=0;barley=1;exit=2)" << std::endl;	
+	std::cin >> choose;
+	switch (choose)
+	{
+	case 0: Chess_Game();
+	case 1:	Barley_Game();
+	case 2: exit(0);			
+	}
+}
