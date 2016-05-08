56fa478b7544919d381acb28dff607d00e0e1fbf
Sergio1C <nofxsss@gmail.com>
2016-05-09 02:30:17 +0300

    add piece_type in piece, add derived piece Kind.

--- Piece.h
+++ Piece.h
@@ -1,23 +1,26 @@
 #ifndef PIECE_H
 #define PIECE_H
 #include "Point.h"
-#include <typeinfo.h>
 
 enum Piece_color { black, white};
+enum Piece_type {pawn,knight,kind};
 
 class Piece
 {
+
+friend ostream& operator<<(ostream&, const Piece*);
+
 public:
-	Piece(Piece_color color);
-	bool virtual chek_move(const Point& from, const Point& to) const=0;
+	Piece(Piece_color color, Piece_type _type);
+	virtual ~Piece() {}
+	virtual bool chek_move(const Point& from, const Point& to) const = 0;
 	Piece_color get_color() const;
-	string virtual print() const=0;
 private:
 	Piece_color _color;
-	friend ostream& operator<<(ostream&, const Piece*);
+	Piece_type _type;
+	virtual string print() const = 0;
 };
 
-//пешка
 class Pawn : public Piece
 {
 public:
@@ -25,11 +28,19 @@ public:
 	bool chek_move(const Point& from, const Point& to) const;
 	string print() const;
 };
-//конь
+
 class Knight : public Piece
 {
 public:
 	Knight(Piece_color color);
+	bool chek_move(const Point& from, const Point& to) const;
+	string print() const;
+};
+
+class Kind : public Piece
+{
+public:	
+	Kind(Piece_color color);
 	bool chek_move(const Point& from, const Point& to) const;
 	string print() const;
 };
