dfd675d8bbf6f4d5031615ad5368b56e1f1a37db
Sergio1C <nofxsss@gmail.com>
2016-05-09 03:13:28 +0300

    1

--- Game.h
+++ Game.h
@@ -9,6 +9,7 @@ class Game{
 public:
 	Game() : cur_color_move(white) {};
 	virtual void move(const Piece*, const Point& from, const Point& to) {};
+	virtual void InitDesk() {}; //initialize desk
 	virtual void ChoiseOfPlayer(); //action in game
 	virtual bool EndOfGame();
 protected:
@@ -23,9 +24,12 @@ private:
 
 class ChessGame : public Game
 {
+friend Piece_type piece_type(const Point& p);
+
 public:
 	ChessGame();
-	void move(const Piece* p, const Point& from, const Point& to);
+	virtual void InitDesk(Desk<8>& desk); //можно ли прописать в таком варианте template? не хочу зависеть на числе "<8>"
+	virtual void move(const Piece* p, const Point& from, const Point& to);
 private:
 	static const int CHESS_COLUMN = 2;
 	static const int FIGURES_PER_ROW = 8;	
